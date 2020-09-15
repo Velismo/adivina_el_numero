9217876459161460baa29a369cc11bc6cb6a30ef
Juan Manuel Cantero Diego <juanmanuelcantero@gmail.com>
2020-09-15 15:27:11 +0200

    creating the walls

--- app.py
+++ app.py
@@ -11,3 +11,30 @@ pName = input()
 #Lets use the library random in a new variable that receive a random number between 1 and 20
 number = random.randint(1, 20)
 print('¡Bienvenido ' + pName + '! Estoy pensando un número entre 1 y 20')
+
+#And here comes the magic. First let's check how many guesses do we have
+while guessCounter < 6:
+    print('Di un número.')
+    guess=input()
+    guess=int(guess)
+
+    guessCounter = guessCounter + 1
+
+#Now we check if the number guessed is bigger or lower than the random one
+    if guess < number:
+        print('Tu número es más bajo.')
+    
+    if guess > number:
+        print('Tu número es más alto.')
+
+    if guess == number:
+        break
+
+#Out of the loop we check if we guess the correct number or not
+if guess == number:
+    guessCounter = str(guessCounter)
+    print('¡Excelente, '+ pName + '! Has adivinado el número que estaba pensando en ' + guessCounter + 'intentos.')
+
+if guess != number:
+    number = str(number)
+    print('Lo siento, ' + pName + '. El número que estaba pensando es el ' + number + '.')
